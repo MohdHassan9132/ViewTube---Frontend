e4e4b95fed33112511733bfc59f23be8b24d9a61
MohdHassan9132 <qureshiayaan36@gmail.com>
2026-02-11 18:27:15 +0530

    fix import paths for consistency in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Pages/Auth/Login";
-import Register from "./pages/Auth/Register";
+import Register from "./Pages/Auth/Register";
 import ProtectedRoute from "./routes/ProtectedRoutes";
 import AppLayout from "./layouts/AppLayout";
-import Home from "./pages/Home/Home";
-import VideoView from "./pages/VideoView/VideoView";
-import ChannelPage from "./pages/Channel/ChannelPage";
-import LikedVideos from "./pages/LikedVideos/LikedVideos";
-import WatchHistory from "./pages/WatchHistory/WatchHistory";
-import YourVideos from "./pages/YourVideos/YourVideos";
-import YourTweets from "./pages/YourTweets/YourTweets";
-import PublishVideo from "./pages/PublishVideo/PublishVideo";
-import UpdateVideo from "./pages/PublishVideo/UpdateVideo";
-import SubscribersPage from "./pages/Subscribers/SubscribersPage";
+import Home from "./Pages/Home/Home";
+import VideoView from "./Pages/VideoView/VideoView";
+import ChannelPage from "./Pages/Channel/ChannelPage";
+import LikedVideos from "./Pages/LikedVideos/LikedVideos";
+import WatchHistory from "./Pages/WatchHistory/WatchHistory";
+import YourVideos from "./Pages/YourVideos/YourVideos";
+import YourTweets from "./Pages/YourTweets/YourTweets";
+import PublishVideo from "./Pages/PublishVideo/PublishVideo";
+import UpdateVideo from "./Pages/PublishVideo/UpdateVideo";
+import SubscribersPage from "./Pages/Subscribers/SubscribersPage";
 import YourPlaylists from "./Pages/YourPlaylist/YourPlaylists";
-import PlaylistView from "./pages/PlaylistView/PlaylistView";
-import UpdateProfile from "./pages/Auth/UpdateProfile";
-import ChangePassword from "./pages/Auth/ChangePassword";
+import PlaylistView from "./Pages/PlaylistView/PlaylistView";
+import UpdateProfile from "./Pages/Auth/UpdateProfile";
+import ChangePassword from "./Pages/Auth/ChangePassword";
 
 function App() {
   return (
